295324dc501ea01cbd18f07f42a914da9c88d028
Ann <anna.mirosshkina.11@mail.ru>
2026-06-02 14:03:42 +0300

    Add reverse_number function

--- numbers_lib.py
+++ numbers_lib.py
@@ -2,9 +2,10 @@ def sum_digits(number):
     sum = 0
     while number > 0:
         sum += number % 10
-        number = number // 10
+        number //= 10
     print(sum)
     return sum
+
 sum_digits(123)
 
 def count_digits(number):
@@ -14,45 +15,65 @@ def count_digits(number):
         count += 1
     print(count)
     return count
+
 count_digits(123)
 
 def max_digit(number):
-    numbers = []
+    num = []
     while number > 0:
-        num = number % 10
-        numbers.append(num)
+        digit = number % 10
+        num.append(digit)
         number //= 10
-    maximum = max(numbers)
+    maximum = max(num)
     print(maximum)
     return maximum
+
 max_digit(123)
 
 def min_digit(number):
-    numbers = []
+    num = []
     while number > 0:
-        num = number % 10
-        numbers.append(num)
-        number //= 10
-    minimum = min(numbers)
+        digit = number % 10
+        num.append(digit)
+        number //=10
+    minimum = min(num)
     print(minimum)
     return minimum
-max_digit(123)
+
+min_digit(123)
 
 def is_even(number):
     if number % 2 == 0:
-        result = 'even'
+        print("является четным")
+        return "even"
     else:
-        result = 'odd'
-    print(result)
-    return result
+        print("является нечетным")
+        return "odd"
+
 is_even(123)
 
 def multiply_digits(number):
     prod = 1
     while number > 0:
-        num = number % 10
-        prod*=num
+        digit = number % 10
+        prod *= digit
         number //= 10
     print(prod)
     return prod
 multiply_digits(123)
+
+def reverse_number(number):
+    num = []
+    while number > 0:
+        digit = number % 10
+        num.append(str(digit))
+        number //= 10
+    result = "".join(num)
+    print(result)
+
+reverse_number(1245)
+
+
+
+
+
